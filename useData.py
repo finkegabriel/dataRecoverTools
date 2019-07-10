96188cfc30d3a183498d7f837560dd4bc8cb1ca9
finkegabriel <finkegabriel@gmail.com>
2019-07-10 11:59:34 -0700

    worksgit add .!

--- useData.py
+++ useData.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import os
-
+import time
 
 def main():
     with open("../scripts/runList.txt") as my_file:
         for x in my_file:
             print(x)
+            os.system(x)
+            time.sleep(2)
 
 main()
