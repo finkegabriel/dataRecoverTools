3d3ad80be8502d88e7337163d8687e629602a515
finkegabriel <finkegabriel@gmail.com>
2019-07-10 11:37:06 -0700

    working script

--- scanFiles.py
+++ scanFiles.py
@@ -5,18 +5,17 @@ import time
 import subprocess
 
 def main():
-    file = []
-
     os.system("cd ../ && ls | grep HPdisk001 > scripts/files.txt")    
     time.sleep(1)
     with open("../scripts/files.txt","r") as my_file:           
         for x in my_file:
             print(x)
             i = f"cd ../ && ls | grep {x}"
-            cmd = f"cd ../ && cat {x} | strings > scripts/{x}"
-            fs = f"{cmd} > scripts/runList.txt"
-            print(fs)
-            os.system(fs)
+            cmd = f"cd ../ && cat {x} | strings > scripts/{x}\n"
+            f = open("../scripts/runList.txt","a")
+            print(cmd)
+            f.write(cmd) 
+            f.close()
             time.sleep(2)
             
 main()
